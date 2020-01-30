05668885de47b2c5e4908e878a346b6d7bd9532e
高威 <weigao@iflytek.com>
2020-01-30 17:35:08 +0800

    add case

--- two-sum/main.go
+++ two-sum/main.go
@@ -23,14 +23,24 @@ https://leetcode-cn.com/problems/two-sum/submissions/
 
 func twoSum(nums []int, target int) []int {
 	index := []int{-1, -1}
-	sumSet := make(map[int]interface{})
+	sumSet := make(map[int][]int)
 	for i, k := range nums {
-		sumSet[k] = i
+		if sumSet[k] == nil {
+			t := make([]int, 0)
+			sumSet[k] = t
+		}
+		sumSet[k] = append(sumSet[k], i)
 	}
 	for i, k := range sumSet {
+		if len(k) > 1 && i+i == target {
+			return k
+		}
+		if target-i == i {
+			continue
+		}
 		num := sumSet[target-i]
 		if num != nil {
-			return []int{k.(int),num.(int)}
+			return []int{k[0], num[0]}
 		}
 	}
 
@@ -39,4 +49,8 @@ func twoSum(nums []int, target int) []int {
 
 func main() {
 	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSum([]int{3, 3}, 6))
+	fmt.Println(twoSum([]int{3, 0, 1, 2, 3}, 6))
+	fmt.Println(twoSum([]int{1,3,4,2}, 6))
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))
 }
